aea0487d742c51d7089cb473f71aa214af442c13
CanadianCrafter <bryan335667879@gmail.com>
2021-01-09 11:57:15 -0500

    Added a basic implementation of a kdTree to help movement

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -5,8 +5,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 import javax.swing.*;
-
-
+import edu.wlu.cs.levy.CG.*;
 public class MainGUI extends JFrame implements ActionListener, MouseListener {
 	//gui stuff
 	JPanel screen = new JPanel();
@@ -15,15 +14,24 @@ public class MainGUI extends JFrame implements ActionListener, MouseListener {
 	
 	//boids
 	ArrayList<Boids> boids = new ArrayList<Boids>();
+	KDTree kd;
+	
+	//settings
+	int distance;
+	double seperationCoefficient;
+	double alignmentCoefficient;
+	double cohesionCoefficient;
 
 	// constructor method
-	public MainGUI() {
+	public MainGUI() throws KeySizeException, KeyDuplicateException {
 		frameSetup();
 		panelDesign();
+		kd = new KDTree(2);
+		startingBoids(20);//temporary
 	}
 
 	//set up the frame
-	private void frameSetup() {
+	public void frameSetup() {
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setSize(screenX, screenY);
 		setTitle("Boids");
@@ -31,13 +39,13 @@ public class MainGUI extends JFrame implements ActionListener, MouseListener {
 		add(screen);
 		setVisible(true);
 		
-//		setResizable(false);
+		setResizable(false); //remove later
 		repaint();
 		
 	}
 
 	//set up the panel
-	private void panelDesign() {
+	public void panelDesign() {
 		//add features for the screen
 		screen.setBorder(null);
 		screen.setBounds(0, 0, screenX, screenY);
@@ -48,10 +56,55 @@ public class MainGUI extends JFrame implements ActionListener, MouseListener {
 
 	}
 	
+
+	public void startingBoids(int amount) throws KeySizeException, KeyDuplicateException {
+		for(int i =0;i<amount;i++) {
+			addBoid(Math.random()*screenX,Math.random()*screenY);
+			kd.insert(getDoubleArray(boids.get(i).position.data), boids.get(i));
+			
+		}
+	}
 	
-	private void addBoid(int x, int y) {
+	public void move() throws KeySizeException, IllegalArgumentException, KeyMissingException, KeyDuplicateException {
+		for(int i =0;i<boids.size();i++) {
+			double coords[] = getDoubleArray(boids.get(i).position.data);
+			ArrayList<Boids> newBoids = (ArrayList<Boids>) kd.nearest(coords,distance);
+			kd.delete(coords);
+			boids.get(i).updateVelocity(newBoids, seperationCoefficient, alignmentCoefficient, cohesionCoefficient);
+			boids.get(i).updatePosition();
+			kd.insert(getDoubleArray(boids.get(i).position.data), boids.get(i));
+		}
+		
+		kd = new KDTree(2);
+		for(int i =0;i<boids.size();i++) {
+			kd.insert(getDoubleArray(boids.get(i).position.data), boids.get(i));
+		}
+		
+	}
+	
+	
+	
+	
+	public void addBoid(double x, double y) {
 		boids.add(new Boids(new Vector(x,y),new Vector(0,0)));
 	}
+	
+	public void draw(Graphics g) {
+		for(int i =0;i<boids.size();i++) {
+			double x = boids.get(i).position.data.get(0);
+			double y = boids.get(i).position.data.get(1);
+			g.drawLine((int)x,(int)y,(int)x,(int)y);
+		}
+	}
+	
+	public double[] getDoubleArray(ArrayList<Double> arraylist) {
+		double array[] = new double[arraylist.size()];
+		for(int i =0;i<arraylist.size();i++) {
+			array[i]=arraylist.get(i);
+		}
+		return array;
+	}
+
 	
 	
 	
